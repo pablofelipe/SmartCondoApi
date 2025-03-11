e76e76a66b54e5a3472b9d5c31715e693a7155b2
Pablo Felipe <pablofelipe@gmail.com>
2025-03-11 14:31:00 -0300

    - Added Condominium and Tower Controllers

---
+++ Controllers/CondominiumController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartCondoApi.Models;
+
+namespace SmartCondoApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CondominiumController(SmartCondoContext _context) : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<IEnumerable<Condominium>> Get()
+        {
+            return await _context.Condominiums.ToListAsync();
+        }
+    }
+}
---
+++ Controllers/TowerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartCondoApi.Models;
+
+namespace SmartCondoApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class TowerController(SmartCondoContext _context) : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<IEnumerable<Tower>> Get()
+        {
+            return await _context.Towers.ToListAsync();
+        }
+
+        //// Obter uma torre por ID
+        //[HttpGet("{id}")]
+        //[Authorize]
+        //public async Task<IActionResult> GetTower(int id)
+        //{
+        //    var tower = await _context.Towers.FindAsync(id);
+        //    if (tower == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(tower);
+        //}
+
+        [HttpGet("byCondominium/{condominiumId}")]
+        public async Task<IActionResult> GetTowersByCondominium(int condominiumId)
+        {
+            var towers = await _context.Towers.Where(t => t.CondominiumId == condominiumId).ToListAsync();
+
+            if (towers == null || towers.Count == 0)
+            {
+                return NotFound("Nenhuma torre encontrada para o condomínio especificado.");
+            }
+
+            return Ok(towers);
+        }
+    }
+}
